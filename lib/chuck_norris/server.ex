6d9ae2ffaeb8ab6734b5de3da346f0268a48570b
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-03-24 17:54:12 -0600

    Add update_info and get_jokes server functions.

--- lib/chuck_norris/server.ex
+++ lib/chuck_norris/server.ex
@@ -2,15 +2,52 @@ defmodule ChuckNorris.Server do
   @moduledoc """
   Server to consulme `ChuckNorris.API`
   """
+
+  @interval :timer.hours(1)
+
   def start_link() do
-    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+    GenServer.start_link(__MODULE__, %{categories: [], jokes: []}, name: __MODULE__)
   end
 
   def init(_state) do
-    {:ok, refresh_state()}
+    new_state = refresh_state()
+    Process.send_after(self(), :refresh_state, @interval)
+    {:ok, new_state}
   end
 
-  def refresh_state do
+  @doc """
+  Refresh state
+  """
+  def update_info do
+    GenServer.cast(__MODULE__, :refresh_state)
+  end
+
+  @doc """
+  Get jokes
+  """
+  def get_jokes do
+    GenServer.call(__MODULE__, :get_jokes)
+  end
+
+  def handle_cast(:refresh_state, _state) do
+    new_state = refresh_state()
+    {:noreply, new_state}
+  end
+
+  def handle_call(:get_jokes, _from, state) do
+    {:reply, state[:jokes], state}
+  end
+
+  def handle_info(:refresh_state, _state) do
+    schedule_refresh()
+    {:noreply, refresh_state()}
+  end
+
+  defp schedule_refresh do
+    Process.send_after(self(), :refresh_state, @interval)
+  end
+
+  defp refresh_state do
     categories =
       Task.async(fn -> ChuckNorris.API.categories() end)
       |> Task.await()
