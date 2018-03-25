6e80bc5d51f79168567bb6104123c63848019007
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-03-24 21:32:33 -0600

    New version with refresh interval on the env. config of the application.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ChuckNorris.MixProject do
   def project do
     [
       app: :chuck_norris,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: "Api to consume `https://api.chucknorris.io/`",
